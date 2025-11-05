e0025ec554e54eb2c36899d680d76bbe38b482b9
Nguyen Phuc Khanh <khanh.nguyenbusiness@hcmut.edu.vn>
2025-11-05 15:00:43 +0700

    update ex8

--- STM32Project/LAB3/Core/Src/processing_fsm.c
+++ STM32Project/LAB3/Core/Src/processing_fsm.c
@@ -4,6 +4,7 @@
  *  Created on: Nov 5, 2025
  *      Author: khanh
  */
+
 #include "processing_fsm.h"
 
 enum FSM_STATE fsmState = 0;
@@ -169,6 +170,21 @@ void fsmProcessing(void) {
 		}
 		if (buttonPressed(0)) {
 			fsmReInit(FSM_GREEN_MOD);
+		}
+		if (buttonPressed(1)) {
+			led7segNumbers[1] ++;
+			if (led7segNumbers[1] >= pow(10, LED7SEG_DIGIT_NUMBER)) {
+				led7segNumbers[1] = 0;
+			}
+		}
+		if (buttonPressed(2)) {
+			led7segNumbers[1] --;
+			if (led7segNumbers[1] < 0) {
+				led7segNumbers[1] = pow(10, LED7SEG_DIGIT_NUMBER) - 1;
+			}
+		}
+		if (buttonPressed(3)) {
+			trafficAmberDuration = led7segNumbers[1];
 		}
 		break;
 	case FSM_GREEN_MOD:
